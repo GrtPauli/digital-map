3113527bfc04175e15708d4b2a7e3df9bf3a2137
Paul Andrew <paulandrew2004@gmail.com>
2024-09-30 12:30:04 +0100

    using custom locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import MapWithPlaceAutocomplete from "./components/Map"
+// import MapWithPlaceAutocomplete from "./components/Map";
+import { useQuery, gql } from "@apollo/client";
+import Map from "./components/MapV3";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const GET_LOCATIONS = gql`
+  query {
+    findSchools(query: {}) {
+      _id
+      title
+      institutionType
+      placeId
+      address
+      latitude
+      longitude
+    }
+  }
+`;
 
 function App() {
+  const { loading, data } = useQuery(GET_LOCATIONS);
+  console.log(data);
+  
 
   return (
     <div>
-      <MapWithPlaceAutocomplete/>
+      {loading ? (
+        <div className="flex flex-col flex-1 justify-center items-center h-screen">
+          <LoadingOutlined className="text-5xl text-green-500" />
+        </div>
+      ) : (
+        <Map locations={data?.findSchools}/>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const client = new ApolloClient({
+  uri: 'https://ntdia-nestjs-api-production.up.railway.app/graphql',
+  cache: new InMemoryCache(),
+})
+
+const root = createRoot(document.getElementById('root')!)
+root.render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
+  </ApolloProvider>,
 )
+
